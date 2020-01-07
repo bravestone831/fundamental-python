c08230350f80f0977d67148f059fe1467116d763
bravestone831 <59324128+bravestone831@users.noreply.github.com>
2020-01-07 13:49:31 +0800

    Add files via upload

---
+++ 02-BIF(input).py
@@ -0,0 +1,9 @@
+print('welcome to our class')
+a = input('please tell the insturctor your name:')
+print("I'm happy to see you,",a)
+
+temp = int(input("Now let's begin our first class. Enter a number between 1-100: "))
+if temp>100 or temp<1:
+    print("don't you understand what I said?")
+else:
+    print("good job")
